7b6ea8ff8ce757fc08be09cec7ad85cf4cb7efe8
Cesar Murillo <namiayumis@gmail.com>
2022-11-13 06:28:16 +0900

    [ADD] check

--- pages/api/check.js
+++ pages/api/check.js
@@ -1,1 +1,34 @@
+import {
+  AzureKeyCredential,
+  DocumentAnalysisClient,
+} from "@azure/ai-form-recognizer";
 
+// set `<your-key>` and `<your-endpoint>` variables with the values from the Azure portal.
+const key = process.env["FORM_RECOGNIZER_API_KEY"];
+const endpoint = process.env["FORM_RECOGNIZER_ENDPOINT"];
+
+// sample document
+const businessCardURL =
+  "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-REST-api-samples/master/curl/form-recognizer/business-card-english.jpg";
+
+const BusinessCard = async (req, res) => {
+  const client = new DocumentAnalysisClient(
+    endpoint,
+    new AzureKeyCredential(key)
+  );
+
+  const poller = await client.beginAnalyzeDocument(
+    "prebuilt-businessCard",
+    businessCardURL
+  );
+
+  const {
+    documents: [result],
+  } = await poller.pollUntilDone();
+
+  if (result) {
+    res.status(200).json({ result });
+  }
+};
+
+export default BusinessCard;
