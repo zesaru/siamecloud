9a9ffb20fe32a3ebac7b9c984f39bad455b267ee
Cesar Murillo <namiayumis@gmail.com>
2022-11-13 13:08:11 +0900

    [ADD] supabase ui helpers

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { useState } from "react";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
 
-export default MyApp
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  );
+}
+export default MyApp;
