a24a009dd20d8528dd63d0249ec0ec85d26ea67d
Cesar Murillo <namiayumis@gmail.com>
2023-01-11 16:57:12 +0900

    [delete] useEffect in hello

--- pages/hello.js
+++ pages/hello.js
@@ -1,24 +1,7 @@
-import { supabase } from "../utils/supabaseClient";
-import { useEffect } from "react";
+import React from "react";
 
 const hello = () => {
-  useEffect(() => {
-    const subscription = supabase
-      .channel("custom-insert-channel")
-      .on(
-        "postgres_changes",
-        { event: "INSERT", schema: "public", table: "contactos" },
-        (payload) => {
-          console.log("Change received!", payload);
-        }
-      )
-      .subscribe();
-
-    // Llamar a unsubscribe cuando se desmonte el componente o se cambie el valor de una dependencia
-    return () => {
-      subscription.unsubscribe();
-    };
-  }, []); // Pasar un arreglo vacío como segundo argumento para que el hook solo se ejecute una vez
+  return <div>hello</div>;
 };
 
 export default hello;
